622fa02ef9b9fa1c0c96af13f54a5bdcab3a0a0f
pschloss <pschloss@umich.edu>
2016-07-19 16:18:45 -0400

    Add script to launch jobs for each environment type

---
+++ code/fire_off_cluster_jobs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+LIBRARY=$1
+
+FRACTIONS=(0_2 0_4 0_6 0_8 1_0)
+REPS=(01 02 03 04 05 06 07 08 09 10)
+
+for F in ${FRACTIONS[@]}
+do
+
+for R in ${REPS[@]}
+do
+cat head8.batch > $LIBRARY.$F.$R.qsub
+echo "make data/mice/mice.$F.$R.accuracy.list;
+make data/mice/mice.$F.$R.an.list;
+make data/mice/mice.$F.$R.f1score.list;
+make data/mice/mice.$F.$R.fn.list;
+make data/mice/mice.$F.$R.mcc.list;
+make data/mice/mice.$F.$R.nn.list;
+make data/mice/mice.$F.$R.swarm.list;
+make data/mice/mice.$F.$R.vagc_1.list;
+make data/mice/mice.$F.$R.vdgc_1.list;
+make data/mice/mice.$F.$R.vagc_8.list;
+make data/mice/mice.$F.$R.vdgc_8.list;
+make data/mice/mice.$F.$R.uagc.list;
+make data/mice/mice.$F.$R.udgc.list;
+make data/mice/mice.$F.$R.vdgc_split5_1.list;
+make data/mice/mice.$F.$R.vdgc_split5_8.list;
+make data/mice/mice.$F.$R.an_split5_1.list;
+make data/mice/mice.$F.$R.an_split5_8.list;
+make data/mice/mice.$F.$R.mcc_split5_1.list;
+make data/mice/mice.$F.$R.mcc_split5_8.list;
+make data/mice/mice.$F.$R.otuclust.list;
+make data/mice/mice.$F.$R.sumaclust.list;" >> $LIBRARY.$F.$R.qsub
+
+cat tail.batch >> $LIBRARY.$F.$R.qsub
+qsub $LIBRARY.$F.$R.qsub
+rm $LIBRARY.$F.$R.qsub
+done
+
+done
