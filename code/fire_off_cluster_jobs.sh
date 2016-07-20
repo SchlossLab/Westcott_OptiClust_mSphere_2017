d8d91637f247784783983486e27258292a555361
pschloss <pschloss@umich.edu>
2016-07-20 13:17:01 -0400

    Fix path to data directory

--- code/fire_off_cluster_jobs.sh
+++ code/fire_off_cluster_jobs.sh
@@ -11,27 +11,27 @@ do
 for R in ${REPS[@]}
 do
 cat head8.batch > $LIBRARY.$F.$R.qsub
-echo "make data/mice/mice.$F.$R.accuracy.list;
-make data/mice/mice.$F.$R.an.list;
-make data/mice/mice.$F.$R.f1score.list;
-make data/mice/mice.$F.$R.fn.list;
-make data/mice/mice.$F.$R.mcc.list;
-make data/mice/mice.$F.$R.nn.list;
-make data/mice/mice.$F.$R.swarm.list;
-make data/mice/mice.$F.$R.vagc_1.list;
-make data/mice/mice.$F.$R.vdgc_1.list;
-make data/mice/mice.$F.$R.vagc_8.list;
-make data/mice/mice.$F.$R.vdgc_8.list;
-make data/mice/mice.$F.$R.uagc.list;
-make data/mice/mice.$F.$R.udgc.list;
-make data/mice/mice.$F.$R.vdgc_split5_1.list;
-make data/mice/mice.$F.$R.vdgc_split5_8.list;
-make data/mice/mice.$F.$R.an_split5_1.list;
-make data/mice/mice.$F.$R.an_split5_8.list;
-make data/mice/mice.$F.$R.mcc_split5_1.list;
-make data/mice/mice.$F.$R.mcc_split5_8.list;
-make data/mice/mice.$F.$R.otuclust.list;
-make data/mice/mice.$F.$R.sumaclust.list;" >> $LIBRARY.$F.$R.qsub
+echo "make data/$LIBRARY/$LIBRARY.$F.$R.accuracy.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.an.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.f1score.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.fn.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.mcc.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.nn.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.swarm.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.vagc_1.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_1.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.vagc_8.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_8.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.uagc.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.udgc.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_split5_1.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_split5_8.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.an_split5_1.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.an_split5_8.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.mcc_split5_1.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.mcc_split5_8.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.otuclust.list;
+make data/$LIBRARY/$LIBRARY.$F.$R.sumaclust.list;" >> $LIBRARY.$F.$R.qsub
 
 cat tail.batch >> $LIBRARY.$F.$R.qsub
 qsub $LIBRARY.$F.$R.qsub
@@ -39,3 +39,4 @@ rm $LIBRARY.$F.$R.qsub
 done
 
 done
+
