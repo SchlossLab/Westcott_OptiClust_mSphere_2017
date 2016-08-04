a544fa4dd93377078d12f092490712981214130d
Pat Schloss <pschloss@umich.edu>
2016-08-04 08:50:24 -0400

    Generalize code to generate other file types

--- code/fire_off_cluster_jobs.sh
+++ code/fire_off_cluster_jobs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LIBRARY=$1
+TYPE=$2
 
 FRACTIONS=(1_0 0_8 0_6 0_4 0_2)
 REPS=(01 02 03 04 05 06 07 08 09 10)
@@ -11,27 +12,27 @@ do
 for R in ${REPS[@]}
 do
 cat head8.batch > $LIBRARY.$F.$R.qsub
-echo "make data/$LIBRARY/$LIBRARY.$F.$R.accuracy.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.an.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.f1score.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.fn.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.mcc.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.nn.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.swarm.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.vagc_1.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_1.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.vagc_8.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_8.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.uagc.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.udgc.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_split5_1.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_split5_8.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.an_split5_1.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.an_split5_8.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.mcc_split5_1.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.mcc_split5_8.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.otuclust.list;
-make data/$LIBRARY/$LIBRARY.$F.$R.sumaclust.list;" >> $LIBRARY.$F.$R.qsub
+echo "make data/$LIBRARY/$LIBRARY.$F.$R.accuracy.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.an.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.f1score.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.fn.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.mcc.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.nn.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.swarm.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.vagc_1.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_1.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.vagc_8.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_8.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.uagc.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.udgc.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_split5_1.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.vdgc_split5_8.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.an_split5_1.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.an_split5_8.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.mcc_split5_1.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.mcc_split5_8.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.otuclust.$TYPE;
+make data/$LIBRARY/$LIBRARY.$F.$R.sumaclust.$TYPE;" >> $LIBRARY.$F.$R.qsub
 
 cat tail.batch >> $LIBRARY.$F.$R.qsub
 qsub $LIBRARY.$F.$R.qsub
@@ -39,4 +40,3 @@ rm $LIBRARY.$F.$R.qsub
 done
 
 done
-
