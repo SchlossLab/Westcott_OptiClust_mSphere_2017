b5eb8eaee8b2b123f4be2d92863ea01547a661cf
Pat Schloss <pschloss@umich.edu>
2016-09-30 15:08:06 -0400

    Have code bring in timing and sobs data

--- code/aggregate_steps.R
+++ code/aggregate_steps.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 #this function concatenates together the dataset, fraction, replicate number,
 #and method to generate the filename stub
 cat_cols_files <- function(x){
@@ -16,13 +18,19 @@ build_steps_files <- function(x){
 
 
 #return the total number of steps to reach point where change is less than #0.01% or to convergence (0.00% change)
-get_required_steps <- function(file){
+get_required_steps <- function(file, time){
 	steps <- read.table(file=file, header=T)
 
 	mcc_a <- steps[-nrow(steps),"mcc"]
 	mcc_b <- steps[-1, "mcc"]
 
-	c(min(steps$iter[(mcc_b - mcc_a) / mcc_a < 0.0001]), max(steps$iter))
+	threshold <- which.min((mcc_b - mcc_a) / mcc_a > 0.0001) + 1
+	extra_time <- sum(steps$time[(threshold+1):nrow(steps)])
+	full_time <- time[file,"cluster_secs"]
+	partial_time <- full_time-extra_time
+
+	c(steps$iter[threshold], steps$num_otus[threshold], partial_time,
+		 max(steps$iter), steps$num_otus[nrow(steps)], full_time)
 }
 
 # Six datasets - two synthetic and four biological
@@ -48,8 +56,15 @@ method_vector <- rep(methods, each=n_reps, times=n_fracs*n_datasets)
 
 results <- data.frame(dataset=dataset_vector, frac=frac_vector, rep=rep_vector, method=method_vector, stringsAsFactors=F)
 
+cluster_data <- read.table(file="data/processed/cluster_data.summary", header=T,
+													stringsAsFactors=FALSE)
+timings <- cluster_data %>%
+							filter(method %in% methods, frac==1) %>%
+							select(dataset, frac, rep, method, cluster_secs)
+rownames(timings) <- apply(timings[1:4], 1, build_steps_files)
+
 steps_names <- apply(results, 1, build_steps_files)
-all_stats <- t(sapply(steps_names, get_required_steps))
+all_stats <- t(sapply(steps_names, get_required_steps, time=timings))
 results$close_enough <- all_stats[,1] #within 0.01%
 results$convergence <- all_stats[,2] #complete convergence
 
