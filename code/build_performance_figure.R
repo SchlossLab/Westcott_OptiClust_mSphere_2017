4ef3f42b14b8bd2d3974618b61eb9bc4bca50f14
Pat Schloss <pschloss@umich.edu>
2016-10-03 15:02:59 -0400

    Build performance plot as a faceted figure

--- code/build_performance_figure.R
+++ code/build_performance_figure.R
@@ -1,7 +1,8 @@
-library(dplyr)
-library(ggplot2)
-library(cowplot)
-library(wesanderson)
+library(dplyr, warn.conflicts=F)
+library(tidyr, warn.conflicts=F)
+library(ggplot2, warn.conflicts=F)
+library(cowplot, warn.conflicts=F)
+library(wesanderson, warn.conflicts=F)
 
 basic_methods <- c('an', 'fn',
 									'nn', 'otuclust', 'sumaclust', 'swarm',
@@ -16,17 +17,23 @@ datasets <- c('soil', 'marine', 'mice', 'human', 'even', 'staggered')
 pretty_datasets <- c("Soil", "Marine", "Mice", "Human", "Even", "Staggered")
 names(pretty_datasets) <- datasets
 
+metrics <- c("mcc", "num_otus", "cluster_secs")
+pretty_metrics <- c("Matthew's\nCorrelation Coefficient", "Number of OTUs", "Time to Cluster\n(seconds)")
+names(pretty_metrics) <- metrics
+
+
 data <- read.table(file="data/processed/cluster_data.summary", header=T)
 
+data$cluster_secs[data$cluster_secs > 5000] <- NA
+
 subset <- data %>%
+			select(dataset, frac, rep, method, cluster_secs, mcc, num_otus) %>%
 			filter(frac==1.0 & method %in% basic_methods) %>%
-			group_by(dataset, method) %>%
-			summarize(avg_mcc=mean(mcc, na.rm=T),
-								sd_mcc=sd(mcc, na.rm=T),
-								avg_sobs=mean(num_otus, na.rm=T),
-								sd_sobs=sd(num_otus, na.rm=T),
-								n=sum(!is.na(mcc))) %>%
-			mutate(cov_mcc=sd_mcc/avg_mcc)
+			gather(metric, value, mcc, num_otus, cluster_secs) %>%
+			group_by(dataset, method, metric) %>%
+			summarize(avg=mean(value, na.rm=T),
+								min=min(value, na.rm=T),
+								max=max(value, na.rm=T))
 
 method_ordering <- data %>%
 			filter(frac==1.0 & method %in% basic_methods) %>%
@@ -36,57 +43,49 @@ method_ordering <- data %>%
 
 subset$method <- factor(subset$method, method_ordering$method)
 subset$dataset <- factor(subset$dataset, datasets)
+subset$metric <- factor(subset$metric, metrics)
 
 my_theme <- theme_classic() +
-	theme(axis.text.x =element_blank(),
-		axis.text.y=element_text(size=8),
-		axis.ticks.x = element_blank(),
-		axis.title.y=element_text(size=8),
+	theme(
+		axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5, size=7),
+		axis.text.y=element_text(size=7),
+		axis.title.y=element_text(size=7),
 		panel.grid.major = element_blank(),
 		panel.grid.minor = element_blank(),
 		panel.border = element_rect(color = "black", fill=NA, size=1),
 		panel.background = element_rect(fill=NA),
-		legend.position="none"
+		strip.background = element_blank()
 	)
 
-plot_performance <- function(x, var, label, ylimits){
-	mcc <- ggplot(x, aes_string("method", var, col="dataset", shape="dataset")) +
-		geom_point(position = position_dodge(0.5), size=2) +
-		geom_vline(xintercept=0.5 + (1:(length(levels(x$method))-1))) +
-		coord_cartesian(ylim=ylimits) +
-		ylab(label) +
-		xlab(NULL) +
-		scale_x_discrete(breaks=levels(subset$method),
-			labels=pretty_methods[levels(subset$method)]) +
-		scale_color_manual(breaks=datasets, labels=pretty_datasets, values=
-			c('black', wes_palette("Darjeeling")))+
-		scale_shape_manual(breaks=datasets, labels=pretty_datasets,
-			guide=guide_legend(override.aes=aes(size=2)),
-			values = c(15, 16, 17, 21, 22, 23))+
-		my_theme
 
-	if(var != 'avg_sobs'){
-		mcc <- mcc + theme(axis.title.y=element_text(margin=margin(0,10,0,0)))
-	}
-	return(mcc)
-}
+facet_labels <- as_labeller(pretty_metrics)
 
-mcc <- plot_performance(subset, 'avg_mcc', "Matthew's\nCorrelation Coefficient", c(0,1))
-cov <- plot_performance(subset, 'cov_mcc', "Coefficient of\nVariation for MCC", c(0,0.04))
-sobs <- plot_performance(subset, 'avg_sobs', "Number of OTUs", c(0,1.05e5))
-
-my_legend <- theme(
-	legend.position = c(0.12, 0.6),
-	legend.title=element_text(size=0),
-	legend.text = element_text(size=5),
-	legend.key.size = unit(0.55, "line"),
-	legend.key = element_rect(fill = NA, linetype=0),
-	legend.margin = unit(-2,"line")
-)
+figure <- ggplot(subset, aes(method, avg, col=dataset, shape=dataset))+
+	geom_point(position = position_dodge(0.5), size=2) +
+	geom_errorbar(position = position_dodge(0.5), aes(ymin=min, ymax=max), width=0.2) +
+	expand_limits(y=c(0,1)) +
+	geom_vline(xintercept=0.5 + (1:(length(levels(subset$method))-1))) +
+	facet_grid(metric ~ ., scales='free_y', switch = 'y', labeller= facet_labels, margins=0) +
+	scale_x_discrete(breaks=levels(subset$method),
+		labels=pretty_methods[levels(subset$method)]) +
+	scale_color_manual(breaks=datasets, labels=pretty_datasets, values=
+		c('black', wes_palette("Darjeeling")), name=NULL)+
+	scale_shape_manual(breaks=datasets, labels=pretty_datasets,
+		values = c(15, 16, 17, 21, 22, 23), name=NULL,
+		guide = guide_legend(override.aes = list(linetype = "blank")))+
+	xlab(NULL) + ylab(NULL) +
+	my_theme +
+	theme(legend.title=element_text(size=7),
+		legend.text = element_text(size=6),
+		legend.key.size = unit(0.55, "line"),
+ 		legend.key = element_rect(fill = NA, linetype=0),
+		legend.position=c(.65, .65), plot.margin=margin(1,1,1,0, unit="mm"),
+		legend.title=element_text(lineheight=-1),
+		legend.background = element_rect(fill="white"),
+		plot.margin=margin(1,1,1,0, unit="mm"))
+figure
 
 ggdraw() +
-	draw_plot(mcc,0,0.7,1,0.3) +
-	draw_plot(cov + my_legend, 0,0.40,1,0.3) +
-	draw_plot(sobs + 	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5, size=8)), 0,0.02,1,0.39) +
-	draw_plot_label(c("A", "B", "C"), x=c(0,0,0), y=c(1.00,0.72,0.42), size=12) +
-	ggsave('results/figures/performance.tiff', width=4, height=5.0, unit='in')
+	draw_plot(figure) +
+	draw_plot_label(c("A", "B", "C"), x=c(0,0,0), y=c(1.00,0.71,0.41), size=12) +
+	ggsave('results/figures/performance.tiff', width=4, height=5.5, unit='in')
