12d860b400cedc14b412ef49bc8529326c2b4449
Pat Schloss <pschloss@umich.edu>
2016-10-10 12:08:43 -0400

    Finish first pub-quality version of scaling figure

--- code/build_scaling_figure.R
+++ code/build_scaling_figure.R
@@ -3,76 +3,138 @@ library(tidyr, quietly=T, warn.conflicts=F)
 library(ggplot2, quietly=T, warn.conflicts=F)
 library(wesanderson, quietly=T, warn.conflicts=F)
 library(cowplot, quietly=T, warn.conflicts=F)
-library(scales)
-basic_methods <- c('mcc')#,'sumaclust', 'udgc','vdgc_1')
-pretty_methods <- c('Average\nNeighbor', 'OptiClust')#, 'Sumaclust',
-										#'DGC with\nUSEARCH', 'DGC with\nVSEARCH')
+
+basic_methods <- c('mcc')
+pretty_methods <- c('OptiClust')
 names(pretty_methods) <- basic_methods
 
-datasets <- c('soil', 'marine', 'mice', 'human')#, 'even', 'staggered')
-pretty_datasets <- c("Soil", "Marine", "Mice", "Human")#, "Even", "Staggered")
+datasets <- c('soil', 'marine', 'mice', 'human')
+pretty_datasets <- c("Soil", "Marine", "Mice", "Human")
 names(pretty_datasets) <- datasets
 
-metrics <- c("cluster_secs")#, "cluster_kb")
-pretty_metrics <- c("Time to Cluster\n(seconds)")#, "Memory Required to\nCluster (kb)")
+metrics <- c("cluster_secs", "cluster_kb")
+pretty_metrics <- c("Time for Each\nClustering Step (seconds)", "Memory Required\n(GB)")
 names(pretty_metrics) <- metrics
 
 
-data <- read.table(file="data/processed/cluster_data.summary", header=T)
+speed_data <- read.table(file="data/processed/mcc_steps.summary", header=T)
 
-scaling <- data %>%
-			select(dataset, frac, rep, method, cluster_secs, cluster_kb) %>%
-			filter(method %in% basic_methods, dataset %in% datasets) %>%
-			gather(metric, value, cluster_secs, cluster_kb) %>%
-			group_by(frac, dataset, metric) %>%
-			summarize(avg=mean(value, na.rm=T),
-								min=min(value, na.rm=T),
-								max=max(value, na.rm=T))
+speed_summary <- speed_data %>%
+			select(dataset, frac, full_secs_cluster, full_steps) %>%
+			filter(dataset %in% datasets) %>%
+			group_by(frac, dataset) %>%
+			summarize(avg=mean(full_secs_cluster/full_steps, na.rm=T),
+								min=min(full_secs_cluster/full_steps, na.rm=T),
+								max=max(full_secs_cluster/full_steps, na.rm=T))
 
-secs <- scaling %>% filter(metric == "cluster_secs") %>%
-			ggplot() +
-			geom_line(aes(frac, avg, color=dataset))+
-			geom_point(aes(frac, avg, color=dataset))+
-			geom_errorbar(aes(frac, ymin=min, ymax=max, col=dataset), width=0.02)+
-			coord_cartesian(xlim = c(0, 1), ylim=c(0,3000))
+speed_models <- speed_data %>%
+		filter(dataset %in% datasets) %>%
+		select(dataset, frac, full_secs_cluster, full_steps) %>%
+		group_by(dataset) %>%
+		do(model = nls((full_secs_cluster/full_steps) ~ b * frac ^ z, start = list(b = 1000, z = 2), data= .))
 
-kb <- scaling %>% filter(metric == "cluster_kb") %>%
-			ggplot() +
-			geom_line(aes(frac, avg, color=dataset))+
-			geom_point(aes(frac, avg, color=dataset))+
-			geom_errorbar(aes(frac, ymin=min, ymax=max, col=dataset), width=0.02)+
-			coord_cartesian(xlim = c(0, 1))
+# speed_models[[1,2]]$m$getPars()["z"]	#2.131886
+# speed_models[[2,2]]$m$getPars()["z"]	#2.975044
+# speed_models[[3,2]]$m$getPars()["z"]	#2.687899
+# speed_models[[4,2]]$m$getPars()["z"]	#2.379366
 
 
 
-c_secs <- data %>% filter(method=="mcc") %>% select(dataset, frac, cluster_secs)
 
-subset <- c_secs %>% filter(dataset == "marine")
-nls(cluster_secs ~ b * frac ^ z, start = list(b = 1000, z = 3), subset)
+memory_data <- read.table(file="data/processed/cluster_data.summary", header=T)
 
-subset <- c_secs %>% filter(dataset == "soil")
-nls(cluster_secs ~ b * frac ^ z, start = list(b = 1000, z = 3), subset)
+memory_summary <- memory_data %>%
+		filter(dataset %in% datasets, method=="mcc") %>%
+		mutate(cluster_gb=cluster_kb/1048000) %>%
+		select(dataset, frac, cluster_gb) %>%
+		group_by(frac, dataset) %>%
+		summarize(avg=mean(cluster_gb, na.rm=T),
+							min=min(cluster_gb, na.rm=T),
+							max=max(cluster_gb, na.rm=T))
 
-subset <- c_secs %>% filter(dataset == "human")
-nls(cluster_secs ~ b * frac ^ z, start = list(b = 1000, z = 3), subset)
+memory_models <- memory_data %>%
+		filter(dataset %in% datasets, method == "mcc") %>%
+		mutate(cluster_gb=cluster_kb/1048000) %>%
+		select(dataset, frac, cluster_gb) %>%
+		group_by(dataset) %>%
+		do(model = nls((cluster_gb) ~ b * frac ^ z, start = list(b = 1e6, z = 3), data= .))
 
-subset <- c_secs %>% filter(dataset == "mice")
-nls(cluster_secs ~ b * frac ^ z, start = list(b = 1000, z = 3), subset)
+# memory_models[[1,2]]$m$getPars()["z"]	#1.998879
+# memory_models[[2,2]]$m$getPars()["z"]	#1.97656
+# memory_models[[3,2]]$m$getPars()["z"]	#1.995457
+# memory_models[[4,2]]$m$getPars()["z"]	#1.957556
+
+my_theme <- theme_classic() +
+	theme(
+		axis.text.x = element_blank(),
+		axis.text.y=element_text(size=7, margin=margin(r=2,0,0,0)),
+		axis.title.y=element_text(size=7),
+		axis.title.x=element_text(size=7),
+		panel.grid.major = element_blank(),
+		panel.grid.minor = element_blank(),
+		panel.border = element_rect(color = "black", fill=NA, size=1),
+		panel.background = element_rect(fill=NA),
+		legend.position = "none"
+	)
+
+my_legend <- theme(
+		legend.text = element_text(size=7),
+		legend.key.size = unit(0.55, "line"),
+ 		legend.key = element_rect(fill = NA, linetype=0),
+		legend.position=c(0.2, 0.8),
+		legend.title=element_text(lineheight=-1),
+		legend.background = element_rect(fill="white", color="black"),
+		legend.margin = margin(t=0,4,4,4)
+	)
+
+speed_plot <- speed_summary %>%
+		ggplot() +
+		geom_line(aes(frac, avg, color=dataset)) +
+		geom_point(aes(frac, avg, color=dataset)) +
+		geom_errorbar(aes(frac, ymin=min, ymax=max, col=dataset), width=0.02) +
+		scale_y_sqrt(breaks=c(0,2,4,8,16,32,64,128,256)) +
+		scale_color_manual(
+						breaks=names(pretty_datasets),
+						labels=pretty_datasets,
+						values=rev(c('black', wes_palette("Darjeeling")[1:3])),
+						name=NULL) +
+		scale_shape_manual(
+						breaks=names(pretty_datasets),
+						labels=pretty_datasets,
+						values = rep(16,6),
+						name=NULL) +
+		xlab(NULL) +
+		ylab(pretty_metrics[1]) +
+		my_theme +
+		my_legend
+
+memory_plot <- memory_summary %>%
+	ggplot() +
+	geom_line(aes(frac, avg, color=dataset)) +
+	geom_point(aes(frac, avg, color=dataset)) +
+	geom_errorbar(aes(frac, ymin=min, ymax=max, col=dataset), width=0.02) +
+	scale_y_sqrt(breaks=c(0,1,2,3,4,5,6,7,8), limits=c(0,8)) +
+	scale_color_manual(
+					breaks=names(pretty_datasets),
+					labels=pretty_datasets,
+					values=rev(c('black', wes_palette("Darjeeling")[1:3])),
+					name=NULL) +
+	scale_shape_manual(
+					breaks=names(pretty_datasets),
+					labels=pretty_datasets,
+					values = rep(16,6),
+					name=NULL)+
+	xlab("Fraction of Dataset Analyzed") +
+	ylab(pretty_metrics[2]) +
+	my_theme +
+	theme(
+		axis.text.x = element_text(size=7,margin=margin(t=3,0,0,0)),
+		axis.title.y = element_text(margin=margin(r=13,0,0,0))
+	)
 
 
-c_mem <- data %>% filter(method=="mcc") %>%
-										select(dataset, frac, cluster_kb)
-subset <- c_mem %>% filter(dataset == "marine")
-nls(cluster_kb ~ b * frac ^ z, start = list(b = 1e6, z = 2), subset)
-
-subset <- c_mem %>% filter(dataset == "soil")
-nls(cluster_kb ~ b * frac ^ z, start = list(b = 1e6, z = 2), subset)
-
-subset <- c_mem %>% filter(dataset == "human")
-nls(cluster_kb ~ b * frac ^ z, start = list(b = 1e6, z = 3), subset)
-
-subset <- c_mem %>% filter(dataset == "mice")
-nls(cluster_kb ~ b * frac ^ z, start = list(b = 1e6, z = 3), subset)
-
-#http://stackoverflow.com/questions/35996877/fitting-multiple-nls-functions-with-dplyr
-# consider killing initial loading time
+ggdraw() +
+	draw_plot(speed_plot,  x=0, y=0.535, 1.0, 0.465) +
+	draw_plot(memory_plot, x=0, y=0.0, 1.0, 0.535) +
+	draw_plot_label(c("A", "B"), x=c(0,0), y=c(1.00,0.54), size=12) +
+	ggsave('results/figures/speed_memory.tiff', width=3.5, height=4.0, unit='in')
