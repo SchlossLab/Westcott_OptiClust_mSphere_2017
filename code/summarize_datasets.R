44b163997488cce8bd569cf50d1f9212693acb4e
Pat Schloss <pschloss@umich.edu>
2017-01-31 15:16:06 -0500

    Add number of otus and distances

--- code/summarize_datasets.R
+++ code/summarize_datasets.R
@@ -1,8 +1,20 @@
 dataset <- c('soil', 'marine', 'human', 'mice', 'staggered', 'even')
 
-count_file_names <- paste0('data/', dataset, '/', dataset, '.count_table')
+distances <- read.table(file="data/processed/distance_counts.tsv")
+distances$V2 <- gsub("data/(.*)/(.*).1_0.01.sm.dist", "\\1", distances$V2)
+rownames(distances) <- distances$V2
 
-get_stats <- function(count_file_name) {
+sobs <- read.table(file="data/processed/sobs_counts.tsv")
+sobs <- sobs[grep("1_0.*.mcc.list", x=sobs$V1),]
+sobs$V1 <- gsub("data/(.*)/.*\\.1_0.*", "\\1", sobs$V1)
+sobs_mean <- sobs %>%
+							select(c(1,3)) %>%
+							group_by(V1) %>%
+							summarize(ave=mean(V3))
+
+get_stats <- function(dataset) {
+
+	count_file_name <- paste0('data/', dataset, '/', dataset, '.count_table')
 	count_data <- read.table(file=count_file_name, header=T)
 
 	n_unique_seqs <- nrow(count_data)
@@ -11,11 +23,14 @@ get_stats <- function(count_file_name) {
 	n_samples <- ncol(count_data) - 2
 	if(n_samples == 0) n_samples <- NA
 
-	c(n_samples=n_samples, n_unique_seqs=n_unique_seqs, n_total_seqs=n_total_seqs)
+	n_distances <- distances[dataset, "V1"]
+	n_sobs <- sobs_mean %>% filter(V1==dataset) %>% select(ave) %>% .[[1]]
+	
+	c(n_samples=n_samples, n_unique_seqs=n_unique_seqs, n_total_seqs=n_total_seqs,
+		n_distances=n_distances, n_sobs=n_sobs)
 }
 
-summary_table <- t(sapply(count_file_names, get_stats))
+summary_table <- t(sapply(dataset, get_stats))
 rownames(summary_table) <- dataset
 
 write.table(summary_table, file="data/processed/datasets.summary", quote=F, sep='\t')
-
